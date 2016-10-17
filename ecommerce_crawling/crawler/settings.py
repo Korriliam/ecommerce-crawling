ee087cdbfbb88c97551fc18cd079389ef56bb55a
Korrigan <rainrider37@gmail.com>
2016-10-18 00:03:03 +0200

    Add setting statements

--- ecommerce_crawling/crawler/settings.py
+++ ecommerce_crawling/crawler/settings.py
@@ -1,4 +1,6 @@
-# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+# -*- coding: utf8 -*-
+from django.conf import settings as d_settings
 
 # Scrapy settings for crawler project
 #
@@ -21,9 +23,6 @@ NEWSPIDER_MODULE = 'crawler.spiders'
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
-# Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
-
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
@@ -33,7 +32,7 @@ ROBOTSTXT_OBEY = True
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
@@ -68,18 +67,36 @@ ROBOTSTXT_OBEY = True
 #    'crawler.pipelines.SomePipeline': 300,
 #}
 
-# Enable and configure the AutoThrottle extension (disabled by default)
-# See http://doc.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
-# The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
-# The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
-# The average number of requests Scrapy should be sending in parallel to
-# each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
-# Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
+AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_START_DELAY = 10.0
+AUTOTHROTTLE_MAX_DELAY = 39.0
+AUTOTHROTTLE_DEBUG = True # pour activer l'affichage de statistiques supplémentaires
+
+CONCURRENT_REQUESTS = 1
+
+REDIRECT_MAX_TIMES = 3
+
+DOWNLOAD_TIMEOUT = 180
+
+
+EXTENSIONS = {
+    'scrapy.contrib.corestats.CoreStats': 500,
+    'scrapy.contrib.logstats.LogStats': 500,
+    # 'util.statsToDb.statsToDb': 800,
+    'scrapy.contrib.throttle.AutoThrottle': 900,
+    # 'util.EndMiddleware.VacuumJobdir':900
+}
+
+# DOWNLOADER_MIDDLEWARES = {
+#         'scrapy.contrib.downloadermiddleware.useragent.UserAgentMiddleware': None,
+#         #activation de rotateUserAgents ici. Va attributer user agent different à chaque requête.
+# }
+
+
+RETRY_ENABLED = False # Booléen qui dit si oui on non, si je n'arrive pas à acceder à une page, je réeessaye (non ici)
+                        # Si c'était vrai, on ressayerai à l'infini
+
+ROBOTSTXT_OBEY = False # Our bot don't follow robots.txt recommandations. On ne suis pas les recommandations des robots.Txt.
 
 # Enable and configure HTTP caching (disabled by default)
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
@@ -87,4 +104,19 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_EXPIRATION_SECS = 0
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
-#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+d_settings.configure(
+    DATABASES={
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': '',
+            'USER': 'root',
+            'PASSWORD': 'root',
+            'HOST': 'localhost',
+            }
+    },
+    INSTALLED_APPS=(
+        'ecommerce_crawling',
+    )
+)
